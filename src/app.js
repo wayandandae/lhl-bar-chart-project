4f58a62c838e29e61e496a28b508d0b0ce79fa73
Jack <jkang0724@hotmail.com>
2022-10-18 12:18:36 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// Requirements:
+// 1. Receive an array of numbers or of arrays of numbers to be processed.
+// 2. Receive an object of option properties to be applied on bar chart.
+// 3. Create axes, ticks, title depending on the data range.
+// 4. Draw bar chart and add proper labels with the options applied.
+// 5. (optional)Let user decide which order or colours of stacked data to be displayed.
